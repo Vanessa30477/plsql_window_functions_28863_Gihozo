f5d0eea337d4de4472ec8ef7812380f9d4485053
VanessaGi <vanessagihozo37@gmail.com>
2026-02-05 03:41:19 -0600

    Add inner join query for sales, customers, and inventory

---
+++ SQL_Queries/inner_join.sql
@@ -0,0 +1,7 @@
+SELECT s.Sale_ID,
+       c.C_Name AS Customer_Name,
+       i.Model_Yr,
+       s.Sale_Price
+FROM Sales s
+INNER JOIN Customers c ON s.Customer_ID = c.Customer_ID
+INNER JOIN Inventory i ON s.Car_ID = i.Car_ID;
