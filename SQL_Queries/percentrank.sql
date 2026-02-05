4893d98abf1d0612f87483410aac545d57ec3324
VanessaGi <vanessagihozo37@gmail.com>
2026-02-05 04:16:15 -0600

    Create percentrank.sql

---
+++ SQL_Queries/percentrank.sql
@@ -0,0 +1,8 @@
+SELECT 
+    i.manufacturer AS Car_Type,
+    SUM(s.sale_price) AS Total_revenue,
+    ROUND(PERCENT_RANK() OVER (ORDER BY SUM(s.sale_price)) * 100, 2) AS percent_rank
+FROM sales s
+JOIN inventory i ON s.car_id = i.car_id
+GROUP BY i.manufacturer
+ORDER BY total_revenue DESC;
