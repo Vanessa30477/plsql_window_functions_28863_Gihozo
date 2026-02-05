d199250f90339567e37038ceaabbc70074c4482e
VanessaGi <vanessagihozo37@gmail.com>
2026-02-05 04:20:43 -0600

    Add SQL query for sales profit and running total

---
+++ SQL_Queries/sum.sql
@@ -0,0 +1,12 @@
+SELECT 
+    s.Sale_Date,
+    s.Sale_Price,
+    i.Purchase_Price,
+    (s.Sale_Price - i.Purchase_Price) AS Profit,
+    SUM(s.Sale_Price) OVER (
+        ORDER BY s.Sale_Date
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS Running_Total
+FROM Sales s
+JOIN Inventory i
+    ON s.car_id = i.car_id;
