00fd4daf48d6799ee05e5675812fd0858bd7f131
VanessaGi <vanessagihozo37@gmail.com>
2026-02-05 04:07:55 -0600

    Add SQL query to rank cars by sale price

---
+++ SQL_Queries/rank.sql
@@ -0,0 +1,6 @@
+SELECT i.Manufacturer,
+       i.Model_Yr,
+       s.Sale_Price,
+       RANK() OVER (ORDER BY s.Sale_Price DESC) AS Price_Rank
+FROM Sales s
+JOIN Inventory i ON s.Car_ID = i.Car_ID;
